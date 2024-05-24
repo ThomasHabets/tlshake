e10f3f710d7df4bfa4e636adcab0fd5548311506
Thomas Habets <thomas@habets.se>
2024-05-24 22:18:05 +0100

    Add TLS version options

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use std::sync::Arc;
 #[derive(clap::Parser, Debug)]
 struct Opt {
     #[clap(long)]
+    tls12: bool,
+
+    #[clap(long)]
     tls13: bool,
 
     #[clap()]
@@ -18,7 +21,7 @@ fn doit(config: Arc<rustls::ClientConfig>, host: String, hostport: String) -> Re
     // Connect TCP.
     let start = std::time::Instant::now();
     let mut tcp_stream = std::net::TcpStream::connect(hostport)?;
-    println!("Connect time: {:?}", start.elapsed());
+    println!("Connect time:       {:?}", start.elapsed());
 
     // Handshake.
     let start = std::time::Instant::now();
@@ -26,11 +29,17 @@ fn doit(config: Arc<rustls::ClientConfig>, host: String, hostport: String) -> Re
     while client.is_handshaking() {
         client.complete_io(&mut tcp_stream)?;
     }
-    println!("Handshake time: {:?}", start.elapsed());
+    println!("Handshake time:     {:?}", start.elapsed());
     // https://docs.rs/rustls/latest/rustls/enum.HandshakeKind.html
-    println!("{:?}", client.handshake_kind());
-    println!("{:?}", client.protocol_version());
-    println!("{:?}", client.negotiated_cipher_suite());
+    println!("  Handshake kind:   {:?}", client.handshake_kind().unwrap());
+    println!(
+        "  Protocol version: {:?}",
+        client.protocol_version().unwrap()
+    );
+    println!(
+        "  Cipher suite:     {:?}",
+        client.negotiated_cipher_suite().unwrap()
+    );
     Ok(())
 }
 
@@ -39,10 +48,12 @@ fn main() -> Result<()> {
     let root_store =
         rustls::RootCertStore::from_iter(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
 
-    let versions = if opt.tls13 {
-        [&rustls::version::TLS13]
+    let versions = if opt.tls12 && !opt.tls13 {
+        vec![&rustls::version::TLS12]
+    } else if !opt.tls12 && opt.tls13 {
+        vec![&rustls::version::TLS13]
     } else {
-        [&rustls::version::TLS12]
+        vec![&rustls::version::TLS12, &rustls::version::TLS13]
     };
 
     let config = rustls::ClientConfig::builder_with_protocol_versions(&versions)

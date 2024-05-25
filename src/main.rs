9d66079bc83f00093509860768d443c523c68a07
Thomas Habets <thomas@habets.se>
2024-05-25 18:51:20 +0100

    Add full support for TLS 1.3 resumption
    Apparently TLS 1.3 resumption only works if the first request had
    early data.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpStream;
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{Error, Result};
 use clap::Parser;
 use log::{debug, info};
 use rustls::pki_types::ServerName;
@@ -22,41 +24,94 @@ struct Opt {
     #[clap(long, default_value = "")]
     alpn: String,
 
+    #[clap(long)]
+    http_get: Option<String>,
+
     #[clap()]
     addr: String,
 }
 
-fn doit(config: Arc<rustls::ClientConfig>, host: String, hostport: String) -> Result<()> {
-    let mut client = rustls::ClientConnection::new(config, ServerName::try_from(host)?)?;
+fn doit(
+    config: Arc<rustls::ClientConfig>,
+    host: &str,
+    hostport: &str,
+    request: Option<&str>,
+) -> Result<()> {
+    let mut conn = rustls::ClientConnection::new(config, ServerName::try_from(host)?.to_owned())?;
+
+    println!("Connection");
+    let sent_early = if let Some(req) = request {
+        if let Some(mut early_data) = conn.early_data() {
+            early_data.write_all(req.as_bytes())?;
+            println!("  Attempting early data");
+            true
+        } else {
+            false
+        }
+    } else {
+        false
+    };
 
     // Connect TCP.
-    let start = std::time::Instant::now();
-    let mut tcp_stream = std::net::TcpStream::connect(hostport)?;
-    println!("Connect time:       {:?}", start.elapsed());
+    let tcp_start = std::time::Instant::now();
+    let mut sock = TcpStream::connect(&hostport)?;
+    println!("  Connect time:       {:?}", tcp_start.elapsed());
+    sock.set_nodelay(true)?;
 
     // Handshake.
+    let mut stream = rustls::Stream::new(&mut conn, &mut sock);
     let start = std::time::Instant::now();
-    //let tls_stream = rustls::Stream::new(&mut client, &mut tcp_stream);
-    while client.is_handshaking() {
-        client.complete_io(&mut tcp_stream)?;
-    }
-    println!("Handshake time:     {:?}", start.elapsed());
-    // https://docs.rs/rustls/latest/rustls/enum.HandshakeKind.html
-    println!("  Handshake kind:   {:?}", client.handshake_kind().unwrap());
+    stream.flush()?;
+    println!("  Handshake time:     {:?}", start.elapsed());
     println!(
-        "  Protocol version: {:?}",
-        client.protocol_version().unwrap()
+        "  Handshake kind:     {:?}",
+        stream
+            .conn
+            .handshake_kind()
+            .ok_or(Error::msg("no handshake kind?"))?
     );
     println!(
-        "  Cipher suite:     {:?}",
-        client.negotiated_cipher_suite().unwrap()
+        "  Protocol version:   {:?}",
+        stream
+            .conn
+            .protocol_version()
+            .ok_or(Error::msg("no protocol version?"))?
     );
     println!(
-        "  ALPN protocol     {:?}",
-        client
+        "  Cipher suite:       {:?}",
+        stream
+            .conn
+            .negotiated_cipher_suite()
+            .ok_or(Error::msg("no cipher?"))?
+    );
+    println!(
+        "  ALPN protocol       {:?}",
+        stream
+            .conn
             .alpn_protocol()
             .map(|s| String::from_utf8(s.to_vec()))
     );
+
+    if let Some(req) = request {
+        let start = std::time::Instant::now();
+        if stream.conn.is_early_data_accepted() {
+            println!("  Early data:         accepted");
+        } else {
+            if sent_early {
+                println!("  Early data:         NOT accepted");
+            }
+            stream.write_all(req.as_bytes())?;
+        }
+        let firstline = {
+            let mut r = String::new();
+            BufReader::new(stream).read_line(&mut r)?;
+            r.replace("\r", "").replace("\n", "")
+        };
+        println!("  HTTP first line:    {firstline}");
+        println!("  Request time:       {:?}", start.elapsed());
+    }
+    println!("  Total time:         {:?}", tcp_start.elapsed());
+
     Ok(())
 }
 
@@ -107,7 +162,7 @@ impl rustls::client::ClientSessionStore for TicketStore {
         server_name: ServerName<'static>,
         value: Tls13ClientSessionValue,
     ) {
-        info!("Insert TLS 1.3 ticket: {server_name:?}, {value:?}");
+        info!("Insert TLS 1.3 ticket: {server_name:?}, […]");
         self.inner.insert_tls13_ticket(server_name, value)
     }
     fn take_tls13_ticket(
@@ -115,7 +170,7 @@ impl rustls::client::ClientSessionStore for TicketStore {
         server_name: &ServerName<'static>,
     ) -> Option<Tls13ClientSessionValue> {
         let r = self.inner.take_tls13_ticket(server_name);
-        info!("Take TLS 1.3 ticket: {server_name:?} => {r:?}");
+        info!("Take TLS 1.3 ticket: {server_name:?} => […]");
         r
     }
 }
@@ -154,25 +209,31 @@ fn main() -> Result<()> {
             "Signature algs {:?}",
             crypto.signature_verification_algorithms.mapping
         );
+
         let mut config = rustls::ClientConfig::builder_with_provider(Arc::new(crypto))
             .with_protocol_versions(&versions)?
             .with_root_certificates(root_store)
             .with_no_client_auth();
+
         if !opt.alpn.is_empty() {
             config.alpn_protocols = vec![opt.alpn.bytes().collect()];
         }
-        // config.resumption = rustls::client::Resumption::disabled();
         config.resumption = rustls::client::Resumption::store(Arc::new(TicketStore::new()));
+        config.enable_early_data = true;
+        assert!(config.enable_sni);
+        assert!(config.enable_early_data);
         Arc::new(config)
     };
 
     let host = &opt.addr;
     // TODO: support IPv6 literals.
     let hostport = host.to_owned() + ":" + &opt.port.to_string();
-
-    doit(config.clone(), host.to_string(), hostport.clone())?;
+    let request = opt.http_get.map(|url| {
+	format!(
+            "GET {url} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\nAccept-Encoding: identity\r\n\r\n")
+    });
+    doit(config.clone(), &host, &hostport, request.as_deref())?;
     println!("");
-    doit(config.clone(), host.to_string(), hostport.clone())?;
-    println!("");
+    doit(config.clone(), &host, &hostport, request.as_deref())?;
     Ok(())
 }
